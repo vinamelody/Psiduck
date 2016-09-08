32c4348a40b4b30239990f6ff59c047ede948450
Vina Melody <vina_melody@yahoo.com>
2016-09-08 16:13:39 +0800

    Try to use Quick and Nimble test

---
+++ PsiduckTests/ViewControllerTest.swift
@@ -0,0 +1,13 @@
+//
+//  ViewControllerTest.swift
+//  Psiduck
+//
+//  Created by Vina Melody on 8/9/16.
+//  Copyright © 2016 Vina Rianti. All rights reserved.
+//
+
+import Cocoa
+
+class ViewControllerTest: QuickSpec {
+
+}
