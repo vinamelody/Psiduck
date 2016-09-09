bec71abf51f86fd03c06a1db68653229e8c2b656
Vina Melody <vina_melody@yahoo.com>
2016-09-09 13:58:57 +0800

    - Function for only calling the api (async) - Decoding the json format to PsiReading class - Annotation as array - Added button Refresh action - Added last updated timestamp on button Refresh - Function prettify date

--- Psiduck/ViewController.swift
+++ Psiduck/ViewController.swift
@@ -12,94 +12,186 @@ import MapKit
 import CoreLocation
 import SwiftyJSON
 
-class ViewController: UIViewController {
-    
-    let apiKey = "veTSJLZLu6LhRrNYUFYbQutADtHkBKvr"
-    let apiUrl = "https://api.data.gov.sg/v1/environment/psi"
+public class ViewController: UIViewController {
 
     @IBOutlet weak var map: MKMapView!
+    @IBOutlet weak var btnRefresh: UIButton!
+    public var annotations = [MKPointAnnotation]()
+    var labelTimestamp: String!
     
-    override func viewDidLoad() {
+    override public func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         map.delegate = self
-        getPsiReading()
+        self.mainAction()
+        
     }
-
-    func getPsiReading() {
+    
+    // MARK: Main Action (callable)
+    
+    public func mainAction() {
         
-        let headers = ["api-key": apiKey]
-        
-        Alamofire.request(.GET, apiUrl, headers: headers, parameters: nil).responseJSON { (response) in
+        self.getPsiReadingFromNea { (json) in
             
-            switch response.result {
-            case .Success(let data):
-                //print(data)
+            guard json != nil else {
                 
-                let json = SwiftyJSON.JSON(data)
+                // something error with retrieving data, error should be displayed, do nothing
+                let alert = UIAlertController(title: "Error", message: "Something is error" + "\nHit Refresh button again when you're connected to the Internet.", preferredStyle: UIAlertControllerStyle.Alert)
+                alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+                self.presentViewController(alert, animated: true, completion: nil)
                 
-                let regions = json["region_metadata"]
-                let readings = json["items"][0]
-                
-                for (key, location):(String, JSON) in regions {
-                    if (key != "0") {
-                        //print(key)
-                        self.createMarker(location["name"].stringValue, latitude: location["label_location"]["latitude"].doubleValue, longitude: location["label_location"]["longitude"].doubleValue, readings: readings)
-//                        print(location["name"])
-//                        print(location["label_location"]["latitude"])
-//                        print(location["label_location"]["longitude"])
-//                        print(readings["readings"]["psi_three_hourly"][location["name"].stringValue])
-                    }
-                }
-                
-                
-                
-                //print(readings["readings"]["psi_three_hourly"]["central"])
-                
-            case .Failure(let error):
-                print(error)
+                return
             }
             
+            // Decoding the json will be followed by creating marker on the map
+            self.decodePsiJson(json!)
+            self.addAnnotationToMap()
+            
+            // Add the last updated data on button Refresh
+            self.updateBtnRefreshLabel()
+            // Fetching data completed, hide the loading indicator
+            LoadingOverlay.shared.hideOverlayView()
             
         }
     }
+
+    // MARK: Button Refresh Action
     
-    func createMarker(regionName: String, latitude: Double, longitude: Double, readings: JSON ) {
+    @IBAction func btnRefreshTapped(sender: UIButton) {
         
-        print("region: " + regionName)
-        let psi_24hr = readings["readings"]["psi_twenty_four_hourly"][regionName].stringValue
-        let psi_3hr = readings["readings"]["psi_three_hourly"][regionName].stringValue
-        let pm10_24hr = readings["readings"]["pm10_twenty_four_hourly"][regionName].stringValue
-        let pm10_sub_index = readings["readings"]["pm10_sub_index"][regionName].stringValue
-        let pm25_24hr = readings["readings"]["pm25_twenty_four_hourly"][regionName].stringValue
-        let pm25_sub_index = readings["readings"]["pm25_sub_index"][regionName].stringValue
-        let so2_24hr = readings["readings"]["so2_twenty_four_hourly"][regionName].stringValue
-        let o3_sub_index = readings["readings"]["o3_sub_index"][regionName].stringValue
-        let o3_8hr_max = readings["readings"]["o3_eight_hour_max"][regionName].stringValue
-        let no2_1hr_max = readings["readings"]["no2_one_hour_max"][regionName].stringValue
-        let co_8hr_max = readings["readings"]["co_eight_hour_max"][regionName].stringValue
-        let co_sub_index = readings["readings"]["co_sub_index"][regionName].stringValue
+        self.annotations.removeAll()
+        self.mainAction()
+        
+    }
     
+    func updateBtnRefreshLabel() {
         
-        let readingContent = "PSI Readings:\n" + "24hr: " + psi_24hr + "\n3hr: " + psi_3hr + "\nPM10 24hr: " + pm10_24hr + "\nPM10 Sub Index: " + pm10_sub_index + "\nPM2.5 24hr: " + pm25_24hr + "\nPM2.5 Sub Index: " + pm25_sub_index + "\nSO2 24hr: " + so2_24hr + "\nO3 8hr: " + o3_8hr_max + "\nO3 Sub Index: " + o3_sub_index + "\nNO2 1hr: " + no2_1hr_max + "\nCO 8hr: " + co_8hr_max + "\nCO Sub Index: " + co_sub_index
+        let labelBtn: String = "Refresh\n(Last updated data on " + self.labelTimestamp + ")"
         
-        let location = CLLocationCoordinate2DMake(latitude, longitude)
+        let btnText = NSMutableAttributedString(string: labelBtn)
+        btnText.addAttribute(NSFontAttributeName, value: UIFont.systemFontOfSize(15), range: NSMakeRange(0,7))
+        btnText.addAttribute(NSFontAttributeName, value: UIFont.systemFontOfSize(10), range: NSMakeRange(8,labelBtn.characters.count-8 ))
+        self.btnRefresh.titleLabel?.textAlignment = NSTextAlignment.Center
+        self.btnRefresh.setAttributedTitle(btnText, forState: .Normal)
+    }
+
+    
+    // MARK: Get PSI Reading from NEA, decode it and create markers
+    
+    public func getPsiReadingFromNea(completion: (JSON? -> ())) {
+        
+        let apiKey = "veTSJLZLu6LhRrNYUFYbQutADtHkBKvr"
+        let apiUrl = "https://api.data.gov.sg/v1/environment/psi"
+        let headers = ["api-key": apiKey]
+        
+        
+        Alamofire.request(.GET, apiUrl, headers: headers, parameters: nil).responseJSON { (response) in
+            
+            LoadingOverlay.shared.showOverlay(self.view)
+            
+            switch response.result {
+            case .Success(let data):
+                
+                let json = SwiftyJSON.JSON(data)
+                
+                completion(json)
+                
+            case .Failure(let error):
+        
+                let alert = UIAlertController(title: "Error", message: error.localizedDescription + "\nHit Refresh button again when you're connected to the Internet.", preferredStyle: UIAlertControllerStyle.Alert)
+                alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+                self.presentViewController(alert, animated: true, completion: nil)
+                print(error)
+                
+            }
+        }
+    }
+    
+    func decodePsiJson(json: JSON) {
+        
+        let regions = json["region_metadata"]
+        let psi = json["items"][0]
+        
+        var timestamp: String = ""
+        timestamp = json["items"][0]["update_timestamp"].stringValue
+        
+        self.labelTimestamp = self.prettifyDate(timestamp)
+        
+        for (key, location):(String, JSON) in regions {
+            // key 0 is national
+            if (key != "0") {
+                let reading = PsiReading(regionName: location["name"].stringValue,
+                                         longitude: location["label_location"]["longitude"].doubleValue,
+                                         latitude: location["label_location"]["latitude"].doubleValue,
+                                         psi: psi)
+                self.createMarker(reading)
+            }
+        }
+
+    }
+    
+    
+    func createMarker(psi: PsiReading ) {
+        
+        let readingContent =
+            "PSI Readings:\n\n" + "24hr: " + psi.psi_24hr +
+            "\n3hr: " + psi.psi_3hr +
+            "\nPM10 24hr: " + psi.pm10_24hr +
+            "\nPM10 Sub Index: " + psi.pm10_sub_index +
+            "\nPM2.5 24hr: " + psi.pm25_24hr +
+            "\nPM2.5 Sub Index: " + psi.pm25_sub_index +
+            "\nSO2 24hr: " + psi.so2_24hr +
+            "\nO3 8hr: " + psi.o3_8hr_max +
+            "\nO3 Sub Index: " + psi.o3_sub_index +
+            "\nNO2 1hr: " + psi.no2_1hr_max +
+            "\nCO 8hr: " + psi.co_8hr_max +
+            "\nCO Sub Index: " + psi.co_sub_index
+        
+        let location = CLLocationCoordinate2DMake(psi.latitude, psi.longitude)
         let annotation = MKPointAnnotation()
         annotation.coordinate = location
-        annotation.title = regionName.capitalizedString
+        annotation.title = psi.regionName.capitalizedString
         annotation.subtitle = readingContent
             
+        self.annotations.append(annotation)
         
-        self.map.addAnnotation(annotation)
+    }
+    
+    func addAnnotationToMap() {
         
+        for annotation in self.annotations {
+            self.map.addAnnotation(annotation)
+        }
+    }
+    
+    
+    // MARK: Helper function to produce human readable date string
+    
+    func prettifyDate(timeString: String) -> String {
+        
+        // The format from NEA is "2016-09-08T17:00:00+08:00"
+        
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssSSSZ"
+        dateFormatter.timeZone = NSTimeZone(name: "UTC+8")
+        
+        guard let newTime = dateFormatter.dateFromString(timeString) else {
+            return timeString
+        }
+        
+        dateFormatter.dateFormat = "E, d MMM yyyy HH:mm"
+
+        let newTimeString = dateFormatter.stringFromDate(newTime)
+        
+        return newTimeString
     }
     
 }
 
 extension ViewController: MKMapViewDelegate {
     
-    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+    public func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
         
         if annotation is MKUserLocation {
             return nil
